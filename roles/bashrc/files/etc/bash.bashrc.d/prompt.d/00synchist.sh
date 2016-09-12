f623661a376fe7cc9607e26a00f8d89481c0eefb
MT <mtan.web@gmail.com>
2016-09-12 23:15:56 +0900

    [ticket: #] Bug fix
    For Empty file
    
    ref : ticket link

--- roles/bashrc/files/etc/bash.bashrc.d/prompt.d/00synchist.sh
+++ roles/bashrc/files/etc/bash.bashrc.d/prompt.d/00synchist.sh
@@ -1,8 +1,11 @@
 # ${bashrcdir}/prompt.d/00synchist.sh
 
 _pc_synchist() {
-  history -a
-  history -c
-  history -r
+  [ -s "${HISTFILE:-$HOME/.bash_history}" ] && 
+  [ $HISTFILESIZE -gt 0 ] && {
+    history -a
+    history -c
+    history -r
+  }
   return 0
 }
