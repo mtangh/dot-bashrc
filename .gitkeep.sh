83b891beba07736ca86132a7e288a6739f3e9819
MT <mtan.web+github@gmail.com>
2018-11-04 21:53:31 +0900

    dot-git* update

--- .gitkeep.sh
+++ .gitkeep.sh
@@ -2,92 +2,124 @@
 THIS="${0##*/}"
 CDIR=$([ -n "${0%/*}" ] && cd "${0%/*}" 2>/dev/null; pwd)
 
+THIS="${THIS:-gitkeep.sh}"
+BASE="${BASE}"
+
 basedirs=""
 _rebuild=0
 _verbose=0
 _dry_run=0
 
 usage() {
+  local exitstat="${1:-1}"
   cat <<_USAGE_
-Usage: $THIS [--rebuild] [-d|--dry-run] [dir...]
+Usage: $THIS [OPTION] [dir...]
+
+OPTION:
+-R,--rebuild
+  Remove gitkeep.
+-d,--dry-run
+  Enable dry-run mode.
+-v,--verbose
+  Verbose print.
 
 _USAGE_
-  exit 1
+  exit $exitstat
 }
 
 while [ $# -gt 0 ]
 do
   case "$1" in
-  --rebuild)
-    _rebuild=1
-    ;;
-  --dry-run|-d)
-    _dry_run=1
-    ;;
-  --verbose|-v)
-    _verbose=1
-    ;;
-  --help)
-    usage
-    ;;
-  -*)
-    ;;
+  -R|--rebuild) _rebuild=1 ;;
+  -d|--dry-run) _dry_run=1 ;;
+  -v|--verbose) _verbose=1 ;;
+  -h|--help)    usage 0 ;;
+  -*)           usage 1 ;;
   *)
-    if [ ! -d "${1}" ]
+    if [ -d "${1}" ]
     then
-      echo "${THIS%.*}: no such directory '${1}'." 1>&2
+      # Base dir
+      basedirs="${basedirs}${1}\n"
+    else
+      echo "${BASE}: no such directory '${1}'." 1>&2
       exit 1
-    fi 
-    basedirs="${basedirs}${1}\n"
+    fi
     ;;
   esac
   shift
 done
 
-basedirs="${basedirs:-.}"
+# No unbound vars
+set -u
 
+# Set default '.' (if empty)
+basedirs="${basedirs:-.\n}"
+
+# Work
+base_dir=""
+keep_dir=""
+
+# Rebuild
 [ $_rebuild -ne 0 ] && {
-  if [ $_dry_run -eq 0 ]
-  then
-    findcmd="rm -f"
-  else
-    findcmd="echo"
-  fi
+
+  findcmd=$(
+    [ $_dry_run -eq 0 ] && echo "rm -f"
+    [ $_dry_run -eq 0 ] || echo "echo")
+
   # Remove gitkeep
-  find ${basedirs} \
-       -name ".gitkeep" -a type f \
-       -exec $findcmd {} \; 2>/dev/null
+  printf "%b" "${basedirs}" |sort -u |
+  while read base_dir
+  do
+    find "${base_dir}" \
+      -name ".gitkeep" -a type f \
+      -exec $findcmd {} \; ;
+  done 2>/dev/null
   echo
-}
 
-exec 2>/dev/null
+} # [ $_rebuild -ne 0 ]
 
-printf "%b" "${basedirs}" |
+# Finding dirs
+printf "%b" "${basedirs}" |sort -u |
 while read base_dir
 do
-  echo "${THIS%.*}: Finding '$base_dir'"
-  find "$base_dir" -type d |
-  while read dir
-  do
-    [ $_verbose -ne 0 ] &&
-      echo "${THIS%.*}: #1 Check dir '$dir'"
-    echo "${dir}" |
-    grep -E '^(/.+|\.+|(.*/){0,1}\.(git|svn|cvs)(/.*){0,1})$' 1>&2 &&
-      continue
-    [ $_verbose -ne 0 ] &&
-      echo "${THIS%.*}: #2 Dir '$dir' have a child ?"
-    echo $(ls -1A "${dir}" |wc -l) |grep -vE '^0$' 1>&2 &&
-      continue
-    [ $_verbose -ne 0 ] &&
-      echo "${THIS%.*}: #2 Dir '$dir' is have not child."
-    [ -e "${dir}/.gitkeep" ] &&
-      continue
-    [ $_verbose -ne 0 ] &&
-      echo "${THIS%.*}: #3 Dir '$dir' is gitkeeping."
-    [ $_dry_run -eq 0 ] &&
-      touch "${dir}"/.gitkeep
-    echo "${THIS%.*}: + '$dir'"
-  done
-done
 
+  echo "${BASE}: Finding '$base_dir'."
+
+  find "$base_dir" -type d |sort -u |
+  while read keep_dir
+  do
+
+    [ $_verbose -ne 0 ] &&
+      echo "${BASE}: #1 Check dir '${keep_dir}'"
+
+    echo "${keep_dir}" |
+    egrep '^(/.+|\.+|(.*/){0,1}\.(git|svn|cvs)(/.*){0,1})$' 1>&2 &&
+      continue
+
+    [ $_verbose -ne 0 ] &&
+      echo "${BASE}: #2 Dir '${keep_dir}' have a child ?"
+
+    echo $(ls -1A "${keep_dir}" |wc -l) |
+    egrep -v '^0$' 1>&2 &&
+      continue
+
+    [ $_verbose -ne 0 ] &&
+      echo "${BASE}: #3 Dir '${keep_dir}' is have not child."
+
+    [ -e "${keep_dir}/.gitkeep" ] &&
+      continue
+
+    [ $_verbose -ne 0 ] &&
+      echo "${BASE}: #4 Dir '${keep_dir}' is gitkeeping."
+
+    [ $_dry_run -eq 0 ] &&
+      touch "${keep_dir}"/.gitkeep
+
+    echo "${BASE}: + '${keep_dir}'"
+
+  done
+
+done 2>/dev/null
+
+# End
 exit 0
