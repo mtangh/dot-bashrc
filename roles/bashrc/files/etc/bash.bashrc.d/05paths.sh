6a0b5c9b352ea88909bea40742691db536f42f38
MT <mtan.web+github@gmail.com>
2019-09-09 14:10:49 +0900

    Fix errors

--- roles/bashrc/files/etc/bash.bashrc.d/05paths.sh
+++ roles/bashrc/files/etc/bash.bashrc.d/05paths.sh
@@ -5,8 +5,10 @@
 pathconf="${bashrcdir}/bin/pathconfig"
 
 # path file and directory.
-sys_path_file="${bashrcdir}/paths.d/paths"
-sys_paths_dir="${bashrcdir}/paths.d/include"
+sys_path_file="${bashrcdir}/pathconfig.d/paths"
+sys_paths_dir="${bashrcdir}/pathconfig.d/paths.d"
+ule_path_file="/usr/local/etc/paths"
+ule_paths_dir="/usr/local/etc/paths.d"
 usr_path_file="${HOME}/.paths"
 xdg_path_file="${XDG_CONFIG_HOME:-${HOME}/.config}/paths"
 
@@ -19,40 +21,47 @@ root_or_admin=0
 
 # root ?
 if [ "$UID" = "0" ] ||
-   [[ "`/bin/groups`" =~ (, *| *)(wheel|admin)(, *| *) ]]
+   [[ "$(/bin/groups)" =~ (, *| *)(wheel|admin)(, *| *) ]]
 then
   root_or_admin=1
 fi
 
 # root path setting
-[ $root_or_admin -eq 0 ] &&
-for path_entry in $(
-/bin/ls -1 \
-"${sys_path_file}.root" \
-"${sys_paths_dir}/root"/* \
-2>/dev/null; )
-do
-  [ -f "${path_entry}" -a -x "${path_entry}" ] &&
-  paths_root="${paths_root+${paths_root} }$(/bin/bash ${path_entry})"
-  [ -f "${path_entry}" -a ! -x "${path_entry}" ] &&
-  paths_root="${paths_root+${paths_root} }$(/bin/cat ${path_entry})"
-done 2>/dev/null || :
-
-# Set default if empty
-[ $root_or_admin -eq 0 -a -z "${paths_root}" ] &&
-for path_entry in {/usr,}/sbin
-do
-  [ -d "${path_entry}" ] &&
-  paths_root="${paths_root+${paths_root} }${path_entry}"
-done 2>/dev/null || :
+if [ $root_or_admin -eq 0 ]
+then
+  for path_entry in $(
+  /bin/ls -1 \
+  "${sys_path_file}.root" \
+  "${sys_paths_dir}/root"/* \
+  "${ule_path_file}.root" \
+  "${ule_paths_dir}/root"/* \
+  2>/dev/null; )
+  do
+    [ -f "${path_entry}" -a -x "${path_entry}" ] &&
+    paths_root="${paths_root+${paths_root} }$(/bin/bash ${path_entry})"
+    [ -f "${path_entry}" -a ! -x "${path_entry}" ] &&
+    paths_root="${paths_root+${paths_root} }$(/bin/cat ${path_entry})"
+  done
+  # Set default if empty
+  if [ -z "${paths_root}" ]
+  then
+    for path_entry in {/usr/local,/usr,}/sbin
+    do
+      [ -d "${path_entry}" ] &&
+      paths_root="${paths_root+${paths_root} }${path_entry}"
+    done 
+  fi 2>/dev/null || :
+fi 2>/dev/null || :
 
 # lookup paths file(s)
 for path_entry in $(
 /bin/ls -1 \
 "${sys_path_file}" \
-"${sys_paths_dir}"/{${machine}/,${osvendor}/,${ostype}/,}/ \
+"${sys_paths_dir}"/* \
+"${ule_path_file}" \
+"${ule_paths_dir}"/* \
 {${usr_path_file},${xdg_path_file}}{.${machine},.${osvendor},.${ostype},} \
-{${usr_path_file},${usr_path_file}}.d/{${machine}/,${osvendor}/,${ostype}/,}* \
+{${usr_path_file},${xdg_path_file}}.d{/${machine},/${osvendor},/${ostype},}/* \
 2>/dev/null; )
 do
   [ -f "${path_entry}" -a -x "${path_entry}" ] &&
@@ -62,12 +71,14 @@ do
 done 2>/dev/null || :
 
 # Set default if empty
-[ -z "${paths_dirs}" ] &&
-for path_entry in {/usr,}/bin
-do
-  [ -d "${path_entry}" ] &&
-  paths_dirs="${paths_dirs+${paths_dirs} }${path_entry}"
-done
+if [ -z "${paths_dirs}" ]
+then
+  for path_entry in {/usr/local,/usr,}/bin
+  do
+    [ -d "${path_entry}" ] &&
+    paths_dirs="${paths_dirs+${paths_dirs} }${path_entry}"
+  done
+fi 2>/dev/null || :
 
 # for ${HOME}/bin
 for path_entry in {${HOME},${XDG_CONFIG_HOME:-$HOME/.config}}/{s,.s,,.}bin
--- roles/bashrc/files/etc/bash.bashrc.d/06manpaths.sh
+++ roles/bashrc/files/etc/bash.bashrc.d/06manpaths.sh
@@ -4,19 +4,24 @@
 # path config command
 pathconf="${bashrcdir}/bin/pathconfig"
 
-# man path file and directory.
-sys_manpath_file="/etc/manpaths"
+# manpath file and directory.
+sys_manpath_file="${bashrcdir}/pathconfig.d/manpaths"
+sys_manpaths_dir="${bashrcdir}/pathconfig.d/manpaths.d"
+ule_manpath_file="/usr/local/etc/manpaths"
+ule_manpaths_dir="/usr/local/etc/manpaths.d"
 usr_manpath_file="${HOME}/.manpaths"
-xdg_manpath_file="${XDG_CONFIG_HOME:-${HOME}/.config}/.manpaths"
+xdg_manpath_file="${XDG_CONFIG_HOME:-${HOME}/.config}/manpaths"
 
-# man path entry
+# manpaths entry
 manpaths_dirs=""
 
 # lookup manpaths file(s)
 for manpath_entry in $(
 /bin/ls -1 \
 "${sys_manpath_file}" \
-"${sys_manpaths_dir}"/{${machine}/,${osvendor}/,${ostype}/,}* \
+"${sys_manpaths_dir}"/* \
+"${ule_manpath_file}" \
+"${ule_manpaths_dir}"/* \
 {${usr_manpath_file},${xdg_manpath_file}}{.${machine},.${osvendor},.${ostype}} \
 {${usr_manpath_file},${xdg_manpath_file}}.d/{${machine}/,${osvendor}/,${ostype}/,}* \
 2>/dev/null; )
