47358b8c6796542cf82dda831ccbbdb4568769ec
MT <mtan.web+github@gmail.com>
2020-01-06 22:14:08 +0900

    Update

--- tests/filter_plugins/tests_task.py
+++ tests/filter_plugins/tests_task.py
@@ -10,12 +10,14 @@ class FilterModule(object):
       "tests_task_valid": self.tests_task_valid,
       "tests_task_eq_setup": self.tests_task_eq_setup,
       "tests_task_eq_teardown": self.tests_task_eq_teardown,
+      "tests_tasks_filename": self.tests_tasks_filename,
     }
 
   def tests_task_valid( self, input ):
     try:
       return input is not None and \
-        (self.is_tests_task_setup(input) or self.is_tests_task_teardown(input))
+        ( self.tests_task_eq_setup(input) \
+        or self.tests_task_eq_teardown(input) )
     except:
       return False
 
@@ -32,4 +34,13 @@ class FilterModule(object):
         re.match('^teardown$',(str(input).strip()).lower())
     except:
       return False
+
+  def tests_tasks_filename( self, input ):
+    try:
+      if input is not None and self.tests_task_valid(input):
+        return 'tests-%s.yml' % (str(input).strip()).lower()
+      else:
+        return None
+    except:
+      return None
 
