a8b717f345a3479f35e9bbfe14cd8cbe8fc77561
MT <mtan.web+github@gmail.com>
2020-03-19 08:27:52 +0900

    Update

--- files/etc/bash.bashrc.d/10prompt.sh
+++ files/etc/bash.bashrc.d/10prompt.sh
@@ -5,17 +5,21 @@
 PS1=$([ ${UID:--1} -ne 0 ] && echo '\u@' || :)'\h \W\$ '
 PS2='> '
 PS3='=> '
-PS4='+(${BASH_SOURCE##*/}${LINENO:+:$LINENO}): ${FUNCNAME:+$FUNCNAME(): }'
+PS4='+${BASH_SOURCE:+(${BASH_SOURCE##*/}${LINENO:+:$LINENO}):} ${FUNCNAME:+$FUNCNAME(): }'
 
 # PS* for TERM
 case "${TERM:-}" in
 xterm*)
   CLR=$([ ${UID:--1} -eq 0 ] && echo '1;31' || echo '1;34')
-  PS1="\[\e[${CLR}m\]${PS1}\[\e[0;39m\]"
-  PS2="\[\e[${CLR}m\]${PS2}\[\e[0;39m\]"
-  PS3="\[\e[${CLR}m\]${PS3}\[\e[0;39m\]"
-  PS4="\[\e[${CLR}m\]${PS4}\[\e[0;39m\]"
-  unset CLR
+  CL1="${CLR}"
+  CL2="${CLR}"
+  CL3="${CLR}"
+  CL4="1;30"
+  PS1="\[\e[${CL1}m\]${PS1}\[\e[0;39m\]"
+  PS2="\[\e[${CL2}m\]${PS2}\[\e[0;39m\]"
+  PS3="\[\e[${CL3}m\]${PS3}\[\e[0;39m\]"
+  PS4="\[\e[${CL4}m\]${PS4}\[\e[0;39m\]"
+  unset CLR CL1 CL2 CL3 CL4
   ;;
 *)
   ;;
---
+++ files/etc/bash.bashrc.d/bin/colors_and_formatting.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -u
+THIS="${BASH_SOURCE##*/}"
+BASE="${THIS%.*}"
+CDIR=$([ -n "${BASH_SOURCE%/*}" ] && cd "${BASH_SOURCE%/*}" 2>/dev/null; pwd)
+
+# Background
+for clbg in {40..47} {100..107} 49
+do
+  # Foreground
+  for clfg in {30..37} {90..97} 39
+  do
+    # Formatting
+    for attr in 0 1 2 4 5 7
+    do
+      # Print the result
+      [ "${attr}" = "7" ] && sepr="" || sepr=" | "
+      printf "\e[${attr};${clbg};${clfg}m %-12s \e[0m%s" "^[${attr};${clbg};${clfg}m" "${sepr}"
+    done
+    echo # Newline
+  done
+done
+
+exit 0
